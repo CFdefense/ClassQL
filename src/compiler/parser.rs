fd7d45b3b313c38dfe84ba67c8ae8b6d055f4176
CFdefense <CFdefence@gmail.com>
2025-08-02 23:48:18 -0400

    ascii art crate changes

---
+++ src/compiler/parser.rs
@@ -0,0 +1,7 @@
+/*
+    This module is responsible for parsing the tokens into an AST
+
+    The AST is a tree of nodes that represent the query
+
+    The AST can then be used by the semantic analyzer to check for semantic errors
+*/
