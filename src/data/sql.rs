04cdc3836034dd2ec606e8bb38252f4b423daf6c
CFdefense <CFdefence@gmail.com>
2026-01-03 01:11:58 -0500

    class doc

--- src/data/sql.rs
+++ src/data/sql.rs
@@ -2,5 +2,18 @@
     For sql code execution
 */
 
+/// Class struct
+///
+/// TODO: Will be responsible for storing a class' information
+/// 
+/// Class fields:
+/// --- ---
+/// None -> No fields
+/// --- ---
+///
+/// Implemented Traits:
+/// --- ---
+/// Debug -> Debug trait for Class
+/// --- ---
 #[derive(Debug)]
 pub struct Class {}
